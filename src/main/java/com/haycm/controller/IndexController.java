e3d32b58600833d550b206dbb7a8dbbcc08ee8e8
teartao <809210721@qq.com>
2015-04-02 16:41:25 +0800

    change controller of index ；change @RequestBody to the head of method

--- src/main/java/com/haycm/controller/IndexController.java
+++ src/main/java/com/haycm/controller/IndexController.java
@@ -2,15 +2,23 @@ package com.haycm.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "index";
+    }
+
     @ResponseBody
-    @RequestMapping(value="/index",produces = "text/plain;charset=UTF-8")
-    public String welcome() {
+    @RequestMapping(value="/getJson",produces = "text/plain;charset=UTF-8")
+    public String getJson() {
         JSONObject studentList=new JSONObject();
 
         JSONObject student1 = new JSONObject();
@@ -28,9 +36,9 @@ public class IndexController {
         student3.put("age", "33");
         student3.put("number", "num333");
 
-        studentList.put("student1",student1);
-        studentList.put("student2",student2);
-        studentList.put("student3",student3);
+        studentList.put("studen t1",student1);
+        studentList.put("studen t2",student2);
+        studentList.put("studen t3",student3);
         System.out.println(studentList);
 
         return studentList.toJSONString();
--- src/main/java/com/haycm/controller/other/DekotaAction.java
+++ src/main/java/com/haycm/controller/other/DekotaAction.java
@@ -19,31 +19,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * 基于Restful风格架构测试
  *
  * @author dekota
- * @since JDK1.5
  * @version V1.0
  * @history 2014-2-15 下午3:00:12 dekota 新建
+ * @since JDK1.5
  */
 @Controller
 public class DekotaAction {
 
-    /** 日志实例 */
+    /**
+     * 日志实例
+     */
     private static final Logger logger = Logger.getLogger(DekotaAction.class);
 
+    @ResponseBody
     @RequestMapping(value = "/hello", produces = "text/plain;charset=UTF-8")
-    public @ResponseBody
-    String hello() {
+    public String hello() {
         return "你好！hello";
     }
 
+    @ResponseBody
     @RequestMapping(value = "/say/{msg}", produces = "application/json;charset=UTF-8")
-    public @ResponseBody
-    String say(@PathVariable(value = "msg") String msg) {
+    public String say(@PathVariable(value = "msg") String msg) {
         return "{\"msg\":\"you say:'" + msg + "'\"}";
     }
 
+    @ResponseBody
     @RequestMapping(value = "/personGet/{id:\\d+}", method = RequestMethod.GET)
-    public @ResponseBody
-    Person getPerson(@PathVariable("id") int id) {
+    public Person getPerson(@PathVariable("id") int id) {
         logger.info("获取人员信息id = " + id);
         Person person = new Person();
         person.setName("张三");
@@ -52,37 +54,37 @@ public class DekotaAction {
         person.setId(id);
         return person;
     }
+
     @ResponseBody
     @RequestMapping(value = "/personPost", method = RequestMethod.POST)
-    public
-    JSONObject addPerson(Person person) {
+    public JSONObject addPerson(Person person) {
         logger.info("注册人员信息成功id=" + person.getId());
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("msg", "注册人员信息成功");
         return jsonObject;
     }
 
+    @ResponseBody
     @RequestMapping(value = "/personDel/{id:\\d+}", method = RequestMethod.DELETE)
-    public @ResponseBody
-    Object deletePerson(@PathVariable("id") int id) {
+    public Object deletePerson(@PathVariable("id") int id) {
         logger.info("删除人员信息id=" + id);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("msg", "删除人员信息成功");
         return jsonObject;
     }
 
+    @ResponseBody
     @RequestMapping(value = "/personPut", method = RequestMethod.PUT)
-    public @ResponseBody
-    Object updatePerson(Person person) {
+    public Object updatePerson(Person person) {
         logger.info("更新人员信息id=" + person.getId());
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("msg", "更新人员信息成功");
         return jsonObject;
     }
 
+    @ResponseBody
     @RequestMapping(value = "/personPatch", method = RequestMethod.PATCH)
-    public @ResponseBody
-    List<Person> listPerson(@RequestParam(value = "name", required = false, defaultValue = "") String name) {
+    public List<Person> listPerson(@RequestParam(value = "name", required = false, defaultValue = "") String name) {
 
         logger.info("查询人员name like " + name);
         List<Person> lstPersons = new ArrayList<Person>();
