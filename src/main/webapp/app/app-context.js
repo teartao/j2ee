63d202275d3c268cb501f15fb2bd98e1dbb2991a
teartao <809210721@qq.com>
2015-04-22 17:26:37 +0800

    modify some examples of angularJS

--- src/main/webapp/app/app-context.js
+++ src/main/webapp/app/app-context.js
@@ -9,6 +9,14 @@ define(function () {
         demo001: {
             controller: 'app001/demo001-ctrl',
             templateUrl: 'app001/demo001.html'
+        },
+        demo002: {
+            controller: 'app002/demo002-ctrl',
+            templateUrl: 'app002/demo002.html'
+        },
+        demo003: {
+            controller: 'app003/demo003-ctrl',
+            templateUrl: 'app003/demo003.html'
         }
     };
 
--- src/main/webapp/app/app001/demo001-ctrl.js
+++ src/main/webapp/app/app001/demo001-ctrl.js
@@ -1,30 +1,35 @@
-define(['diamond', './demo001-service'], function (diamond, anyName) {
+define(['diamond', './demo001-service'], function (diamond) {
     diamond.lazy.controller('demo001Controller', function ($rootScope, $scope, $http, $log, demo001Service) {
         loadPage();
 
         function loadPage() {
 
             var personObj = {id: 1, name: '张三', sex: '男', age: 23};
-            demo001Service.personGetRequest().success(function (response) {
-                $log.debug("<<<<<<<<<<<<<<<<");
-                $log.debug("[返回]get person 成功");
-                $log.debug(response);
-            });
+            $scope.doGet = function () {
+                demo001Service.personGetRequest().success(function (response) {
+                    $log.debug("<<<<<<<<<<<<<<<<");
+                    $log.debug("[返回]get person 成功");
+                    $log.debug(response);
+                });
+            };
 
-            demo001Service.personPostRequest(personObj).success(function (response) {
-                $log.debug("<<<<<<<<<<<<<<<<");
-                $log.debug("[返回]post person 成功");
-                $log.debug(response);
-            });
-
-            demo001Service.indexRequest().success(function (response) {
-                $log.debug("<<<<<<<<<<<<<<<<");
-                $log.debug("[返回]getJson 成功");
-                $log.debug(response);
-                $scope.studentList = response;
-            });
+            $scope.doPost = function () {
+                demo001Service.personPostRequest(personObj).success(function (response) {
+                    $log.debug("<<<<<<<<<<<<<<<<");
+                    $log.debug("[返回]post person 成功");
+                    $log.debug(response);
+                });
+            };
 
 
+            $scope.getStudentList = function () {
+                demo001Service.indexRequest().success(function (response) {
+                    $log.debug("<<<<<<<<<<<<<<<<");
+                    $log.debug("[返回]getJson 成功");
+                    $log.debug(response);
+                    $scope.studentList = response;
+                });
+            };
 
         }
     });
