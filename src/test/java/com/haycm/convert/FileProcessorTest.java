afb84c9e94d531928d3ad4b8980a83b09e054481
teartao <809210721@qq.com>
2015-04-22 10:57:31 +0800

    add test for inuyacha

---
+++ src/test/java/com/haycm/convert/FileProcessorTest.java
@@ -0,0 +1,35 @@
+package com.haycm.convert;
+
+import org.hehe.util.FileConvertUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author TaoLei
+ * @Date 2015/4/21.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/applicationContext.xml"})
+public class FileProcessorTest {
+    @Resource
+    private FileConvertUtils fileConvertUtils;
+
+    @Test
+    public void test1() {
+        String htmlPath = "E:\\download\\temp\\test.html";
+        String docPath = "E:\\download\\temp\\newDoc.doc";
+        String docxPath = "E:\\download\\temp\\newDocx.docx";
+        String pdfPath = "E:\\download\\temp\\newDocx.pdf";
+
+        fileConvertUtils.convertHtmlToDOC(htmlPath, docPath);
+        fileConvertUtils.convertHtmlToDOCX(htmlPath, docxPath);
+        fileConvertUtils.convertWordToPDF(docPath, pdfPath);
+        fileConvertUtils.convertHtmlToPDF(htmlPath, pdfPath);
+
+    }
+}
