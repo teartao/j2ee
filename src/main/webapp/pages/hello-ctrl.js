2903228efee6c3f99bd8d5e84f76428839f7729f
taolei <taolei>
2016-07-13 00:38:02 +0800

    add test

--- src/main/webapp/pages/hello-ctrl.js
+++ src/main/webapp/pages/hello-ctrl.js
@@ -2,7 +2,8 @@
  * Created by taolei on 16/7/13.
  */
 var app = angular.module('hello', []);
-app.controller('helloController', function ($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
+app.controller('helloCtrl', function ($scope, $http) {
+    $scope.findGuest = function () {
+        
+    }
 });
