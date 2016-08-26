a5c5ac9acbe10cf192376e498d415527949638d0
taolei <taolei>
2016-08-27 00:00:44 +0800

    add "add guest price "demo

--- src/main/java/com/tao/controller/IndexController.java
+++ src/main/java/com/tao/controller/IndexController.java
@@ -1,17 +1,18 @@
 package com.tao.controller;
 
 import com.alibaba.fastjson.JSONObject;
-import com.tao.entity.Guest;
 import com.tao.service.GuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @Author TaoLei
@@ -27,10 +28,19 @@ public class IndexController {
 
     @ResponseBody
     @RequestMapping(value = "guests", method = RequestMethod.GET)
-    public JSONObject getException(HttpServletRequest request) throws IOException {
+    public JSONObject getGuests(HttpServletRequest request) {
         String keyword = request.getParameter("keyword");
         JSONObject result = new JSONObject();
         result.put("guests", guestService.findGuests(keyword));
         return result;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "addMoney", method = RequestMethod.POST)
+    public JSONObject addMoney(@RequestBody JSONObject param) {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+        System.out.println(df.format(new Date()) + " -- " + param.getString("name") + " -- " + param.getString("price"));
+        return param;
     }
 }
--- src/main/webapp/pages/index-ctrl.js
+++ src/main/webapp/pages/index-ctrl.js
@@ -3,7 +3,39 @@
  */
 var app = angular.module('index', []);
 app.controller('indexCtrl', function ($scope, $http) {
+    $scope.guestList = [];
     $scope.findGuest = function () {
-        $http.get("guests?keyword=" + $scope.keyword);
-    }
+        $http.get("guests?keyword=" + $scope.userName).success(function (response) {
+            $scope.guestList = response.data['guests']
+        });
+    };
+    $scope.selectGuest = function (name) {
+        $scope.userName = name;
+    };
+    $scope.addMoney = function () {
+        if ($scope.userName == null || $scope.userName == '') {
+            alert("请输入客人名!");
+            return;
+        }
+        if ($scope.amount == null || $scope.amount == '') {
+            alert("钱呢??!!");
+            return;
+        }
+        if ($scope.amount % 100 != 0) {
+            alert("礼金居然是零钱 ?");
+            return;
+        }
+        var param = {
+            price: $scope.amount,
+            name: $scope.userName
+        };
+        $http.post("addMoney", param).success(function (response) {
+            if (response.success = true) {
+                var guest = response.data;
+                alert("添加成功");
+                console.log("添加成功 --->> : " + guest.name + " : " + guest.price);
+            }
+        });
+    };
+
 });
