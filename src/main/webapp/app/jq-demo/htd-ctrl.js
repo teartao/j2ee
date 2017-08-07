0f7c74734fb1f7dd4abc7bd1bc5f5282396ec8fc
taolei <l.tao@htd.cn>
2017-08-07 17:20:04 +0800

    finished html of angular

--- src/main/webapp/app/jq-demo/htd-ctrl.js
+++ src/main/webapp/app/jq-demo/htd-ctrl.js
@@ -3,5 +3,13 @@
  */
 var htdModule = angular.module('htd', []);
 htdModule.controller('htdCtrl', function ($scope, $http, $log) {
-
+    $scope.showDetail=0;
+    $scope.menus = [];
+    $http.get("./menu.json").success(function (menuData) {
+        $scope.menus = menuData;
+    });
+    $scope.floors = [];
+    $http.get("./floor.json").success(function (floorData) {
+        $scope.floors = floorData;
+    });
 });
