dcdb35fe91aec107a260b696e72aea6253daece1
taolei <l.tao@htd.cn>
2017-11-21 11:42:28 +0800

    add spring demo

---
+++ ssm/src/main/java/com/tao/controller/HelloController.java
@@ -0,0 +1,41 @@
+package com.tao.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "hello";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "json", method = RequestMethod.GET)
+    public JSONObject getJsonObj() throws IOException {
+        JSONObject result = new JSONObject();
+        result.put("aaa", "呵呵额");
+        result.put("bbb", "222");
+        result.put("ccc", "333");
+        return result;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "getException", method = RequestMethod.GET)
+    public JSONObject getException() throws IOException {
+        throw new RuntimeException("has an exception");
+    }
+}
