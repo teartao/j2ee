ba26e6d48b4cd65cc17d03b56ec3438cf40ea419
taolei <l.tao@htd.cn>
2017-11-28 16:09:07 +0800

    dev

--- javaee/src/main/resources/school_task.sql
+++ javaee/src/main/resources/school_task.sql
@@ -1,9 +1,43 @@
--- 查出所有 94年出生的学生
+-- 所有92年出生的学生
 SELECT  s.* FROM db_student s
 WHERE s.birth LIKE '1992%';
 
--- 查出三年二班所有学生
-select s.*,c.* from db_student s
+-- 三年二班所有学生
+SELECT s.*,c.* FROM db_student s
 LEFT JOIN db_class c on c.id=s.class_id
 WHERE c.grade_no=3 AND c.classno=2;
+
+SELECT s.*,c.* FROM db_student s
+INNER JOIN db_class c on c.id=s.class_id AND c.grade_no=3 AND c.classno=2;
+
+
+-- 二年级每班人数
+select count(s.id) '人数',c.classno '班级' FROM  db_student s
+LEFT JOIN db_class c ON  c.id =s.class_id
+WHERE c.grade_no=2
+GROUP BY c.id
+
+select count(s.id) '人数',c.classno '班级' FROM  db_student s
+INNER JOIN db_class c ON  c.id =s.class_id AND c.grade_no=2
+GROUP BY c.id
+
+
+-- 3年2班所有学生年龄
+select s.*,(YEAR(now())-YEAR(s.birth)) '年龄',c.* from db_student s
+INNER JOIN db_class c on c.id=s.class_id
+WHERE c.grade_no=3 AND c.classno=2;
+
+select s.*,(YEAR(now())-YEAR(s.birth)) '年龄',c.* from db_student s
+INNER JOIN db_class c on c.id=s.class_id AND c.grade_no=3 AND c.classno=2;
+
+
+-- 2-4年级所有学生平均年龄
+SELECT
+c.grade_no '年级',avg((YEAR(now())-YEAR(s.birth)))  '平均年龄'
+FROM db_student s
+INNER JOIN db_class c ON c.id =s.class_id AND (c.grade_no >=2 AND c.grade_no<=4)
+GROUP BY c.grade_no
+
+ --ON  WHERE HAVING 区别 https://www.cnblogs.com/strive-study/p/5068158.html
+
 
