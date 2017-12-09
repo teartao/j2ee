7484931d4af7c2236e2baa822db61efbc32e9ebf
neotao <neotao@126.com>
2017-12-10 00:19:05 +0800

    add util of verify code

---
+++ ssm/src/main/java/com/tao/utils/AuthImage.java
@@ -0,0 +1,33 @@
+package com.tao.utils;
+
+/**
+ * Created by taolei on 2017/12/9.
+ */
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthImage extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+    static final long serialVersionUID = 1L;
+
+    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+
+        //生成随机字串
+        String verifyCode = VerifyCodeUtil.generateVerifyCode(4);
+        //存入会话session
+        HttpSession session = request.getSession(true);
+        session.setAttribute("rand", verifyCode.toLowerCase());
+        //生成图片
+        int w = 200, h = 80;
+        VerifyCodeUtil.outputImage(w, h, response.getOutputStream(), verifyCode);
+
+    }
+}
