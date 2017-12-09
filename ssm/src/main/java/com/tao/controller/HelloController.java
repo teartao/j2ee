e8396e29aa8e24e61fd1ae677216c2e2f857d2d6
neotao <neotao@126.com>
2017-12-10 00:19:27 +0800

    add : regist login  createVerifyCode

--- ssm/src/main/java/com/tao/controller/HelloController.java
+++ ssm/src/main/java/com/tao/controller/HelloController.java
@@ -1,17 +1,27 @@
 package com.tao.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.tao.entity.Result;
+import com.tao.utils.AuthImage;
+import com.tao.utils.VerifyCodeUtil;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class HelloController {
     @Resource
@@ -23,7 +33,6 @@ public class HelloController {
         return "hello";
     }
 
-    @ResponseBody
     @RequestMapping(value = "json", method = RequestMethod.GET)
     public JSONObject getJsonObj() throws IOException {
         JSONObject result = new JSONObject();
@@ -33,9 +42,74 @@ public class HelloController {
         return result;
     }
 
-    @ResponseBody
     @RequestMapping(value = "getException", method = RequestMethod.GET)
     public JSONObject getException() throws IOException {
         throw new RuntimeException("has an exception");
+    }
+
+    @RequestMapping(value = "verifycode/create", method = RequestMethod.GET)
+    public Result<JSONObject> createVerifyCode(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Result<JSONObject> result = new Result<>();
+        String path = "xxx.img";
+        File file = new File(request.getRealPath("/") + path);
+        String verifyCode = setFileResponse(request, response, file);
+        file.createNewFile();
+        JSONObject data = new JSONObject();
+        data.put("path", path);
+        data.put("verifyCode", verifyCode);
+        result.setResult(data, "0000", "生成成功");
+        return result;
+    }
+
+    private String setFileResponse(HttpServletRequest request, HttpServletResponse response, File file) throws IOException {
+        response.setHeader("Pragma", "No-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("image/jpeg");
+
+        //生成随机字串
+        String verifyCode = VerifyCodeUtil.generateVerifyCode(4);
+        //存入会话session
+        HttpSession session = request.getSession(true);
+        session.setAttribute("rand", verifyCode.toLowerCase());
+        //生成图片
+        int w = 200, h = 80;
+        VerifyCodeUtil.outputImage(w, h, file, verifyCode);
+        return verifyCode;
+    }
+
+    @RequestMapping(value = "user/create", method = RequestMethod.GET)
+    public Result<Map> userRegister(HttpServletRequest request) {
+        Result<Map> result = new Result<>();
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        String verifyCode = request.getParameter("verifyCode");
+        String sql = "SELECT * FROM sysuser t WHERE t.username=? AND t.password=?";
+        List<Map<String, Object>> queryRs = jdbcTemplate.queryForList(sql, new Object[]{userName, password});
+        if (queryRs != null && queryRs.size() > 0) {
+            result.setCode("0000");
+            result.setMessage("注册成功");
+        } else {
+            result.setCode("9999");
+            result.setMessage("注册失败");
+        }
+        return result;
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public Result<Map> userLogin(HttpServletRequest request) {
+        Result<Map> result = new Result<>();
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        String sql = "SELECT * FROM sysuser t WHERE t.username=? AND t.password=?";
+        List<Map<String, Object>> queryRs = jdbcTemplate.queryForList(sql, new Object[]{userName, password});
+        if (queryRs != null && queryRs.size() > 0) {
+            result.setCode("0000");
+            result.setMessage("登陆成功");
+        } else {
+            result.setCode("9999");
+            result.setMessage("登陆失败");
+        }
+        return result;
     }
 }
