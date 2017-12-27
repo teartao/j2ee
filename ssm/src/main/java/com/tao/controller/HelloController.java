3a157afa2990a17605b905295685b24ef06257c7
taolei <l.tao@htd.cn>
2017-12-27 18:09:01 +0800

    添加 ueditor

--- ssm/src/main/java/com/tao/controller/HelloController.java
+++ ssm/src/main/java/com/tao/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,9 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -144,5 +147,46 @@ public class HelloController {
         } else {
             return "failed";
         }
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "upload/ueditor")
+    public Map<String, Object> upload(MultipartFile upfile, HttpServletRequest request, HttpServletResponse response) {
+        // 文件读取路径
+        Map<String, Object> params = new HashMap<String, Object>();
+        try {
+            String basePath = "D:/code/universe/ssm/target/ssm-1.0-SNAPSHOT";
+            String visitUrl = "";
+            String fileName = "";
+            if (upfile != null) {
+                //文件原名
+                String ext = upfile != null ? upfile.getOriginalFilename() : "";
+                //生成文件名
+                fileName = String.valueOf(System.currentTimeMillis()).concat(ext);
+
+                //写文件到磁盘或其它指定位置
+                StringBuilder sb = new StringBuilder();
+                sb.append(basePath).append("/").append(fileName);
+                visitUrl = visitUrl.concat(fileName);
+                File f = new File(sb.toString());
+                if (!f.exists()) {
+                    f.getParentFile().mkdirs();
+                }
+                OutputStream out = new FileOutputStream(f);
+                FileCopyUtils.copy(upfile.getInputStream(), out);
+
+                params.put("size", upfile.getSize());
+                params.put("type", upfile.getContentType());
+                params.put("original", fileName);
+                params.put("url", visitUrl);
+                params.put("state", "SUCCESS");
+            }else{
+                params.put("state", "文件不存在");
+            }
+
+        } catch (Exception e) {
+            params.put("state", "ERROR");
+        }
+        return params;
     }
 }
