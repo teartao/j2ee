0fa4cbf5eafdff0317ca5efeb89c4408409bb31c
taolei <l.tao@htd.cn>
2018-05-18 23:41:43 +0800

    new sql

--- htd-lunch-calc/htd_lunch.sql
+++ htd-lunch-calc/htd_lunch.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50560
 File Encoding         : 65001
 
-Date: 2018-05-16 21:37:04
+Date: 2018-05-18 23:38:29
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,37 +21,52 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `menu`;
 CREATE TABLE `menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL COMMENT '菜单名称',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `creater_id` datetime DEFAULT NULL COMMENT '创建者用户id',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
-INSERT INTO `menu` VALUES ('1', 'xxx啊啊', null);
 
 -- ----------------------------
--- Table structure for order_his
+-- Table structure for menu_selection
 -- ----------------------------
-DROP TABLE IF EXISTS `order_his`;
-CREATE TABLE `order_his` (
+DROP TABLE IF EXISTS `menu_selection`;
+CREATE TABLE `menu_selection` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `people_id` int(11) DEFAULT NULL COMMENT '点餐人id',
-  `p_date` datetime DEFAULT NULL COMMENT '点餐日期',
-  `r_id` int(11) NOT NULL COMMENT '菜单recipt id',
+  `name` varchar(255) DEFAULT NULL COMMENT '菜单项 菜名',
+  `menu_id` int(11) DEFAULT NULL COMMENT '所属菜单id',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of order_his
+-- Records of menu_selection
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for people
+-- Table structure for order
 -- ----------------------------
-DROP TABLE IF EXISTS `people`;
-CREATE TABLE `people` (
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL COMMENT '点餐人id',
+  `selection_id` int(11) NOT NULL COMMENT '菜单项 菜名id',
+  `create_time` datetime DEFAULT NULL COMMENT '点餐日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of order
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `dept` varchar(255) DEFAULT NULL,
@@ -59,20 +74,24 @@ CREATE TABLE `people` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of people
+-- Records of user
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for recepit
+-- Table structure for user_wx
 -- ----------------------------
-DROP TABLE IF EXISTS `recepit`;
-CREATE TABLE `recepit` (
+DROP TABLE IF EXISTS `user_wx`;
+CREATE TABLE `user_wx` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `menu_id` int(11) DEFAULT NULL,
+  `nick_name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of recepit
+-- Records of user_wx
 -- ----------------------------
