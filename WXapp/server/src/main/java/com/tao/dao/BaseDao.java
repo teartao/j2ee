b8a33c80fc564ca2635f5a898cc92adce63c7ff3
taolei <neotao@126.com>
2018-05-20 23:40:19 +0800

    mybatis

--- WXapp/server/src/main/java/com/tao/dao/BaseDao.java
+++ WXapp/server/src/main/java/com/tao/dao/BaseDao.java
@@ -1,15 +1,25 @@
 package com.tao.dao;
 
+import org.apache.ibatis.annotations.Param;
+import net.hehe.common.Page;
+
 import java.util.List;
 
 public interface BaseDao<T> {
-    void add(T param);
+    void add(@Param("entity") T param);
 
-    List<T> findList(T param);
+    List<T> findList(@Param("entity") T param ,@Param("page") Page page);
 
-    T find(T param);
+    T find(@Param("entity") T param);
 
-    void update(T param);
+    Integer update(@Param("entity") T param);
 
-    void delete(T param);
+    Integer delete(@Param("entity") T param);
+
+    public T selectById(Object id);
+
+    public Integer updateBySelect(@Param("entity") T t);
+
+    public Long queryCount(@Param("entity") T entity);
+
 }
