463965db0a124b35ff24099fd607ba7e6fce9489
taolei <l.tao@htd.cn>
2018-05-21 10:06:37 +0800

    let const

--- htd-lunch-calc/calc.js
+++ htd-lunch-calc/calc.js
@@ -1,11 +1,11 @@
 function getMenu(menuTxt) {
-    var menu = [];
+    const menu = [];
     if (menuTxt != null && menuTxt !== '') {
-        var menus = menuTxt.split('\n');
-        for (var i = 1; i < menus.length; i++) {
-            var aMenu = menus[i];
-            if (aMenu != null && aMenu != '') {
-                var price = getNumber(aMenu);
+        const menus = menuTxt.split('\n');
+        for (let i = 1; i < menus.length; i++) {
+            const aMenu = menus[i];
+            if (aMenu != null && aMenu.trim() !== '') {
+                const price = getNumber(aMenu);
                 menu.push({
                     name: getCNWord(aMenu),
                     price: price == null ? 10 : price
@@ -18,13 +18,13 @@ function getMenu(menuTxt) {
 
 
 function getOrders(orderTxt) {
-    var orders = [];
+    const orders = [];
     if (orderTxt != null && orderTxt !== '') {
-        var ordersArr = orderTxt.split('\n');
-        for (var i = 0; i < ordersArr.length; i++) {
-            var order = ordersArr[i];
-            if (order != null && order != '') {
-                var count = getNumber(order);
+        const ordersArr = orderTxt.split('\n');
+        for (let i = 0; i < ordersArr.length; i++) {
+            const order = ordersArr[i];
+            if (order != null && order.trim() !== '') {
+                const count = getNumber(order);
                 orders.push({
                     name: getCNWord(order),
                     count: count == null ? 1 : count
@@ -38,23 +38,23 @@ function getOrders(orderTxt) {
 
 
 function calcPrice() {
-    var priceDom = document.getElementById('price');
-    var menuDom = document.getElementById('menu');
-    var orderDom = document.getElementById('order');
+    const priceDom = document.getElementById('price');
+    const menuDom = document.getElementById('menu');
+    const orderDom = document.getElementById('order');
 
-    var menus = getMenu(menuDom.value);
-    var orders = getOrders(orderDom.value);
+    const menus = getMenu(menuDom.value);
+    const orders = getOrders(orderDom.value);
     if (menus == null || menus.length <= 0) {
         alert('填写菜单啊');
         return;
     } else if (orders == null || orders.length <= 0) {
         alert('不点吃个P啊');
     } else {
-        var price = 0;
-        for (var i = 0; i < orders.length; i++) {
-            var order = orders[i];
-            for (var j = 0; j < menus.length; j++) {
-                var menu = menus[j];
+        let price = 0;
+        for (let i = 0; i < orders.length; i++) {
+            const order = orders[i];
+            for (let j = 0; j < menus.length; j++) {
+                const menu = menus[j];
                 if (menu.name.indexOf(order.name) >= 0) {
                     price += (menu.price * order.count);
                     break;
--- htd-lunch-calc/lib/lib.js
+++ htd-lunch-calc/lib/lib.js
@@ -1,10 +1,13 @@
 String.prototype.replaceAll = function (s1, s2) {
     return this.replace(new RegExp(s1, "gm"), s2);
 };
+String.prototype.trim = function() {
+    return this.replace(/(^\s*)|(\s*$)/g, '');
+};
 
 function getCNWord(str) {
-    var cnReg = /[\u4e00-\u9fa5]+.*[\u4e00-\u9fa5]/g;
-    var words = str.match(cnReg);
+    const cnReg = /[\u4e00-\u9fa5]+.*[\u4e00-\u9fa5]/g;
+    const words = str.match(cnReg);
     if (words != null && words.length > 0) {
         return words[0];
     } else {
@@ -13,8 +16,8 @@ function getCNWord(str) {
 }
 
 function getNumber(str) {
-    var cnReg = /[0-9]+/g;
-    var nums = str.match(cnReg);
+    const cnReg = /[0-9]+/g;
+    const nums = str.match(cnReg);
     if (nums != null && nums.length > 0) {
         return Number(nums[0]);
     } else {
