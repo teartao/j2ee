70bf69c3cc5be0d752cebb851ac88ba5c62db13f
taolei <l.tao@htd.cn>
2018-05-21 13:59:00 +0800

    interceptor

--- WXapp/server/src/main/java/com/tao/interceptor/ParameterInterceptor.java
+++ WXapp/server/src/main/java/com/tao/interceptor/ParameterInterceptor.java
@@ -14,18 +14,18 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ParameterInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) throws Exception {
         System.out.println("preHandle");
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object object, ModelAndView modelAndView) throws Exception {
         System.out.println("postHandle");
     }
 
     @Override
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, Exception e) throws Exception {
         System.out.println("afterCompletion");
     }
 }
