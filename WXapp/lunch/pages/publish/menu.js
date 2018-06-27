54abd5faa432e3cfa746bf2d9d2e493a7a8253f0
taolei <l.tao@htd.cn>
2018-06-27 16:47:14 +0800

    xcx

--- WXapp/lunch/pages/publish/menu.js
+++ WXapp/lunch/pages/publish/menu.js
@@ -1,5 +1,12 @@
-// pages/menu/menu.js
+String.prototype.replaceAll = function (s1, s2) {
+  return this.replace(new RegExp(s1, "gm"), s2);
+};
+String.prototype.trim = function () {
+  return this.replace(/(^\s*)|(\s*$)/g, '');
+};
 
+// pages/menu/menu.js
+const app = getApp()
 function getCNWord(str) {
   var cnReg = /[\u4e00-\u9fa5]+.*[\u4e00-\u9fa5]/g;
   var words = str.match(cnReg);
@@ -21,11 +28,17 @@ function getNumber(str) {
 }
 function getMenu(menuTxt) {
   var menu = [];
+  var menuTitle = '';
   if (menuTxt != null && menuTxt !== '') {
     var menus = menuTxt.split('\n');
-    for (var i = 1; i < menus.length; i++) {
+    for (var i = 0; i < menus.length; i++) {
       var aMenu = menus[i];
-      if (aMenu != null && aMenu != '') {
+      if (aMenu != null && aMenu.trim() != '') {
+        if (aMenu.indexOf('月') > 0 && aMenu.indexOf('日') > 0){
+          menuTitle = aMenu;
+          continue;
+        }
+
         var price = getNumber(aMenu);
         menu.push({
           name: getCNWord(aMenu),
@@ -34,7 +47,10 @@ function getMenu(menuTxt) {
       }
     }
   }
-  return menu;
+  return {
+    title:menuTitle,
+    selectionList:menu
+  };
 }
 Page({
 
@@ -49,6 +65,50 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+          console.log('current user :');
+          console.log(res.userInfo);
+        }
+      })
+    }
+
+
+    var $this = this;
+    wx.request({
+      url: 'https://prep-new-vms.htd.cn/hcf/user',
+      method: 'POST',
+      header: {
+        'Content-Type': 'json'
+      },
+      data: JSON.stringify(app.globalData.userInfo),
+      success: function (res) {
+        this.setData({
+          loginUser: res
+        })
+      }
+    })
   
   },
 
@@ -107,7 +167,18 @@ Page({
     console.log(menus)
   },
   submitMenu:function(e){
-
+    var $this = this;
+    wx.request({
+      url: 'https://prep-new-vms.htd.cn/hcf/user',
+      method: 'POST',
+      header: {
+        'Content-Type': 'json'
+      },
+      data: JSON.stringify(app.globalData.userInfo),
+      success: function (res) {
+        console.log('create user success ');
+      }
+    })
   }
 })
 
