47b7ee5073655dc21fde1e965c71b6f8564d12bf
taolei <l.tao@htd.cn>
2018-09-04 08:40:20 +0800

    add refactor todo

---
+++ WXapp/server/src/main/java/com/tao/controller/MenuController.java
@@ -0,0 +1,14 @@
+package com.tao.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author neotao
+ * @Date 2018/9/4
+ * @Version V0.0.1
+ * @Desc
+ */
+@RestController
+public class MenuController {
+    // TODO: 2018/9/4 refactor menu
+}
