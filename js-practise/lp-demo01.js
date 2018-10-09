76e622ecc550bd920d8d068dc71ceb869b00831f
taolei <l.tao@htd.cn>
2018-10-09 19:00:48 +0800

    Delete unused code

--- js-practise/lp-demo01.js
+++
@@ -1,8 +0,0 @@
-Number.prototype.floor = function (scale) {
-    scale = Number(scale);
-    if (isNaN(scale) || scale !== Number(scale.toFixed(0))) {
-        return ;
-    }
-    var ratio = Math.pow(10,scale);
-    return Math.floor(this * ratio) / ratio;
-};
