861bd1b2af10627573a3b67a681acc93a7425bec
taolei <l.tao@htd.cn>
2018-10-09 19:06:02 +0800

    Delete unused code

---
+++ javaee/src/main/java/neo/tao/entity/Result.java
@@ -0,0 +1,48 @@
+package neo.tao.entity;
+
+/**
+ * Created by taolei on 2017/12/9.
+ */
+public class Result<T> {
+    private T data;
+    private String message;
+    private String code;
+
+    public Result() {
+    }
+
+    public Result(T data, String code, String message) {
+        this.data = data;
+        this.code = code;
+        this.message = message;
+    }
+    public void setResult(T data, String code, String message) {
+        this.data = data;
+        this.code = code;
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T result) {
+        this.data = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
